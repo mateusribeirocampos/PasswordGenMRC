bfcfd3da577b902bf871fa1e875864fa1d779858
mateusribeirocampos <mateusrcampos@gmail.com>
2023-08-02 15:47:42 -0300

    Botao limpar e cancelar

---
+++ src/main/java/cursojavabasico/passwordgenmrc/PasswordGenMRC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cursojavabasico.passwordgenmrc;
+
+/**
+ *
+ * @author Mateus Campos
+ */
+public class PasswordGenMRC {
+
+    public static void main(String[] args) {
+        TelaPasswordMRC telaPassword = new TelaPasswordMRC();
+        telaPassword.setVisible(true);
+    }
+}
